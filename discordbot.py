a37ebc7d6a58a2b43f9833fe552bc27e684eccc7
ikasooomen <40800507+ikasooomen@users.noreply.github.com>
2020-08-09 21:41:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -36,6 +36,6 @@ async def on_message(message):
         await message.channnel.send(random.choice(randomlegends) + "を使え！")
     # その他
     elif message.content == "talk":
-        await message.channel.send(random.choice(iikaesi)) 
+        await message.channel.send(random.choice(iikaesi))
 
 bot.run(token)
